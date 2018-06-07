087bac359e33d6f31a2f2215e7ad3559d776d0de
markozeman <mz4730@student.uni-lj.si>
2018-06-07 16:16:44 +0200

    Calculating only vector with matrix - cost: 0.06

--- bool_factorization.py
+++ bool_factorization.py
@@ -54,8 +54,8 @@ class BoolFactorization():
             sample -= self.number_of_cells_A
             return False, sample // self.m, sample % self.m
 
-    def cost(self, m):
-        return np.sum(np.logical_xor(self.matrix, m))
+    def cost(self, my, real):
+        return np.sum(np.logical_xor(my, real))
 
     def print_matrices(self):
         a = ''.join(list(map(lambda x: '1' if x else '0', self.A.reshape(self.n * self.r))))
@@ -65,8 +65,6 @@ class BoolFactorization():
         print()
 
     def simulated_annealing(self):
-        best_cost = self.n * self.m
-        previous_cost = self.cost(self.A.dot(self.B))
         iteration = 0
         while True:
             # get temperature according to time
@@ -74,27 +72,26 @@ class BoolFactorization():
             if temp <= 0:
                 # print('Temp below 0!')
                 # print('Iterations: ', iteration)
-                # print('Best: ', best_cost)
+                # print('Final error: ', round(self.cost(self.matrix, self.A.dot(self.B)) / (self.n * self.m), 4))
                 break
 
             # randomly select cell in one matrix and change it
             first, row, column = self.sample2matrix_position(self.matrix_samples[iteration])
             if first:
+                previous_cost = self.cost(self.A[row, :].dot(self.B), self.matrix[row, :])
                 self.A[row, column] = not self.A[row, column]
+                cost = self.cost(self.A[row, :].dot(self.B), self.matrix[row, :])
             else:
+                previous_cost = self.cost(self.A.dot(self.B[:, column]), self.matrix[:, column])
                 self.B[row, column] = not self.B[row, column]
+                cost = self.cost(self.A.dot(self.B[:, column]), self.matrix[:, column])
 
-            # calculate new A @ B
-            m = self.A.dot(self.B)
-
-            # calculate new cost
-            cost = self.cost(m)
+            # calculate delta cost
             delta = previous_cost - cost
 
             # if cost is better than change the state, else change it with probability proportional to temperature
             if delta > 0:
-                if cost < best_cost:
-                    best_cost = cost
+                if temp < 2:
                     self.print_matrices()
             else:
                 # random between 0 and 1
@@ -111,14 +108,12 @@ class BoolFactorization():
                         self.B[row, column] = not self.B[row, column]
 
             # print('iteration: ', iteration, ' cost: ', cost)
-
-            previous_cost = cost
-            iteration += 1
             # print()
+
+            iteration += 1
 
 
 
 if __name__ == '__main__':
     bf = BoolFactorization()
-
 
